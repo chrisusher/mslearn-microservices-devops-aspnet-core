54d2f9744ecfd1cfa9c758651953a45d36542db9
christopher usher <chris.usher23@gmail.com>
2022-01-12 20:44:32 +0000

    Add new Controller so can get Git Sha by REST API

---
+++ src/Web/WebSPA/Server/Controllers/VersionController.cs
@@ -0,0 +1,27 @@
+﻿// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using eShopOnContainers.WebSPA;
+
+namespace eShopConContainers.WebSPA.Server.Controllers
+{
+    public class VersionController : Controller
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly IOptionsSnapshot<AppSettings> _settings;
+
+        public VersionController(IWebHostEnvironment env, IOptionsSnapshot<AppSettings> settings)
+        {
+            _env = env;
+            _settings = settings;
+        }
+
+        [HttpGet]
+        public IActionResult Git()
+        {
+            return Json(ThisAssembly.Git.Sha);
+        } 
+    }
+}
