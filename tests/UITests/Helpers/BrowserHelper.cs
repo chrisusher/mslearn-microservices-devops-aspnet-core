00c828da377a4e2bdb81eca4e2d28eacbe29d459
christopher usher <chris.usher23@gmail.com>
2022-01-24 20:38:22 +0000

    Clear down Browser Setup code for Presentation

--- tests/UITests/Helpers/BrowserHelper.cs
+++ tests/UITests/Helpers/BrowserHelper.cs
@@ -7,51 +7,7 @@ namespace UITests.Helpers
     {
         public static void SetupBrowser()
         {
-            PlaywrightCommon.PlaywrightInstance = Playwright.CreateAsync().Result;
-
-            switch(Common.Settings.Browser)
-            {
-                case Model.Enums.BrowserType.Chrome:
-                    PlaywrightCommon.Browser = PlaywrightCommon.PlaywrightInstance.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-                    {
-                        Channel = "chrome",
-                        Headless = Common.Settings.Headless
-                    }).Result;
-                    break;
-
-                case Model.Enums.BrowserType.Edge:
-                    PlaywrightCommon.Browser = PlaywrightCommon.PlaywrightInstance.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-                    {
-                        Channel = "msedge",
-                        Headless = Common.Settings.Headless
-                    }).Result;
-                    break;
-
-                case Model.Enums.BrowserType.Firefox:
-                    PlaywrightCommon.Browser = PlaywrightCommon.PlaywrightInstance.Firefox.LaunchAsync(new BrowserTypeLaunchOptions
-                    {
-                        Headless = Common.Settings.Headless
-                    }).Result;
-                    break;
-
-                case Model.Enums.BrowserType.Safari:
-                    PlaywrightCommon.Browser = PlaywrightCommon.PlaywrightInstance.Webkit.LaunchAsync(new BrowserTypeLaunchOptions
-                    {
-                        Headless = Common.Settings.Headless
-                    }).Result;
-                    break;
-            }
-
-            Common.Context = PlaywrightCommon.Browser.NewContextAsync().Result;
-            Common.Context.Tracing.StartAsync(new TracingStartOptions
-            {
-                Screenshots = true,
-                Snapshots = true
-            });
-
-            PlaywrightCommon.Page = Common.Context.NewPageAsync().Result;
-
-            _ = PlaywrightCommon.Page.GotoAsync(Common.Settings.Url).Result;
+            
         }
     }
 }
